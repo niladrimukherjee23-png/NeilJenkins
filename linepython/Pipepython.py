9c7e32521652fa37f0bb46c354e1a3629055bd69
niladrimukherjee23-png <niladri.mukherjee23@gmail.com>
2025-10-29 10:53:38 +0000

    Modify input method to use command line arguments
    Updated the script to accept a name argument from the command line, with a default value if none is provided.

--- linepython/Pipepython.py
+++ linepython/Pipepython.py
@@ -1,7 +1,14 @@
-var1 = input("What is your name?")
-var3 = ["B","C","D"]
+import sys
+
+# Check if an argument is provided for name, otherwise set a default
+if len(sys.argv) > 1:
+    var1 = sys.argv[1]
+else:
+    var1 = "User"  # Default name if no argument is provided
+
+var3 = ["B", "C", "D"]
+
 if any(char in var3 for char in var1):
     print(var1)
 else:
     print("Wrong Name")
-    #This is a demo code
